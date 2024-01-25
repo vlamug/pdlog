ba5b5d64ec84fc654723a15268fb7fe79ca48128
Vladislav Mugultyanov <vmugultyanov@ozon.ru>
2024-01-25 10:58:33 +0400

    segment functionality

---
+++ internal/log/segment.go
@@ -0,0 +1,145 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	log_v1 "github.com/vlamug/pdlog/api/v1"
+	"google.golang.org/protobuf/proto"
+)
+
+type segment struct {
+	store                  *store
+	index                  *index
+	baseOffset, nextOffset uint64
+	config                 Config
+}
+
+func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
+	s := &segment{
+		baseOffset: baseOffset,
+		config:     c,
+	}
+
+	var err error
+	storeFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d.%s", baseOffset, ".store")),
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.store, err = newStore(storeFile); err != nil {
+		return nil, err
+	}
+
+	indexFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d.%s", baseOffset, ".index")),
+		os.O_RDWR|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.index, err = newIndex(indexFile, c); err != nil {
+		return nil, err
+	}
+
+	if off, _, err := s.index.Read(-1); err != nil {
+		s.nextOffset = baseOffset
+	} else {
+		s.nextOffset = baseOffset + uint64(off) + 1
+	}
+
+	return s, nil
+}
+
+func (s *segment) Append(record *log_v1.Record) (offset uint64, err error) {
+	cur := s.nextOffset
+	record.Offset = cur
+	p, err := proto.Marshal(record)
+	if err != nil {
+		return 0, err
+	}
+
+	_, pos, err := s.store.Append(p)
+	if err != nil {
+		return 0, err
+	}
+
+	// @todo extract this line to separate function
+	if err = s.index.Write(uint32(s.nextOffset-s.baseOffset), pos); err != nil {
+		return 0, err
+	}
+
+	s.nextOffset++
+	return cur, nil
+}
+
+func (s *segment) Read(off uint64) (*log_v1.Record, error) {
+	// @todo extract this line to separate function
+	// translate the absolute index into a relative offset and get the associated index entry
+	_, pos, err := s.index.Read(int64(off - s.baseOffset))
+	if err != nil {
+		return nil, err
+	}
+
+	p, err := s.store.Read(pos)
+	if err != nil {
+		return nil, err
+	}
+
+	record := &log_v1.Record{}
+	if err := proto.Unmarshal(p, record); err != nil {
+		return nil, err
+	}
+
+	return record, nil
+}
+
+// IsMaxed checks whether the segment (store or index) has reached its max size. It is used to know whether we need to
+// create new segment.
+//
+// if you write small amount of too big logs, then you would hit the store size limit
+// if you write big amount of too small logs, then you would hit the index size limit
+func (s *segment) IsMaxed() bool {
+	return s.store.size >= s.config.Segment.MaxStoreBytes || s.index.size >= s.config.Segment.MaxIndexBytes
+}
+
+func (s *segment) Remove() error {
+	if err := s.Close(); err != nil {
+		return err
+	}
+
+	if err := os.Remove(s.store.Name()); err != nil {
+		return err
+	}
+
+	if err := os.Remove(s.index.Name()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *segment) Close() error {
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func nearestMultiple(j, k uint64) uint64 {
+	if j >= 0 {
+		return (j / k) * k
+	}
+
+	return ((j - k + 1) / k) * k
+}
