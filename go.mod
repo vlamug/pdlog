06eae88eb7dd2edd7190957e0f8d7036cc89e7ba
Vladislav Mugultyanov <vmugultyanov@ozon.ru>
2024-02-17 19:13:37 +0400

    discovery, members

--- go.mod
+++ go.mod
@@ -4,19 +4,33 @@ go 1.21.5
 
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/stretchr/testify v1.8.0
+	github.com/hashicorp/serf v0.10.1
+	github.com/stretchr/testify v1.8.1
 	github.com/tysonmote/gommap v0.0.2
+	go.uber.org/zap v1.26.0
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/grpc v1.48.0
 	google.golang.org/protobuf v1.28.1
 )
 
 require (
+	github.com/armon/go-metrics v0.0.0-20180917152333-f0300d1749da // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
+	github.com/hashicorp/go-msgpack v0.5.3 // indirect
+	github.com/hashicorp/go-multierror v1.1.0 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/hashicorp/memberlist v0.5.0 // indirect
+	github.com/miekg/dns v1.1.41 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
-	golang.org/x/text v0.3.3 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/discovery/members.go
@@ -0,0 +1,120 @@
+package discovery
+
+import (
+	"net"
+
+	"github.com/hashicorp/serf/serf"
+	"go.uber.org/zap"
+)
+
+const rpsAddrTagKey = "rpc_addr"
+
+type Membership struct {
+	cfg     *Config
+	handler Handler
+
+	serf   *serf.Serf
+	events chan serf.Event
+	logger *zap.Logger
+}
+
+func New(handler Handler, cfg *Config) (*Membership, error) {
+	m := &Membership{
+		cfg:     cfg,
+		handler: handler,
+		logger:  zap.L().Named("membership"),
+	}
+
+	if err := m.setupSerf(); err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (m *Membership) Members() []serf.Member {
+	return m.serf.Members()
+}
+
+func (m *Membership) Leave() error {
+	return m.serf.Leave()
+}
+
+func (m *Membership) setupSerf() error {
+	addr, err := net.ResolveTCPAddr("tcp", m.cfg.BindAddr)
+	if err != nil {
+		return err
+	}
+
+	m.events = make(chan serf.Event)
+
+	cfg := serf.DefaultConfig()
+	cfg.Init()
+	cfg.MemberlistConfig.BindAddr = addr.IP.String()
+	cfg.MemberlistConfig.BindPort = addr.Port
+	cfg.EventCh = m.events
+	cfg.Tags = m.cfg.Tags
+	cfg.NodeName = m.cfg.NodeName
+
+	m.serf, err = serf.Create(cfg)
+	if err != nil {
+		return err
+	}
+
+	go m.eventHandler()
+	if m.cfg.StartJoinAddrs != nil {
+		_, err := m.serf.Join(m.cfg.StartJoinAddrs, true)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *Membership) eventHandler() {
+	for e := range m.events {
+		switch e.EventType() {
+		case serf.EventMemberJoin:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					continue
+				}
+
+				m.handleJoin(member)
+			}
+		case serf.EventMemberLeave, serf.EventMemberFailed:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					return
+				}
+				m.handleLeave(member)
+			}
+		}
+	}
+}
+
+func (m *Membership) handleJoin(member serf.Member) {
+	if err := m.handler.Join(member.Name, member.Tags[rpsAddrTagKey]); err != nil {
+		m.logError(err, "failed to join member", member)
+	}
+}
+
+func (m *Membership) handleLeave(member serf.Member) {
+	if err := m.handler.Leave(member.Name); err != nil {
+		m.logError(err, "failed to leave member", member)
+	}
+}
+
+func (m *Membership) isLocal(member serf.Member) bool {
+	return m.serf.LocalMember().Name == member.Name
+}
+
+func (m *Membership) logError(err error, msg string, member serf.Member) {
+	m.logger.Error(
+		msg,
+		zap.Error(err),
+		zap.String("name", member.Name),
+		zap.String(rpsAddrTagKey, member.Tags[rpsAddrTagKey]),
+	)
+}
